c860638964c522710bcd183e588c50a6efbb741b
vixataaa <vmetodiev93@gmail.com>
2017-10-08 16:39:48 +0300

    Code-first migrations enabled, initial migration

---
+++ src/JobSystem/JobSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace JobSystem.Data.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MsSqlDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(MsSqlDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
