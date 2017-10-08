1857af9562beea3671c78a8c2b51dd573c222c27
vixataaa <vmetodiev93@gmail.com>
2017-10-08 13:15:56 +0300

    Init project

---
+++ src/JobSystem/JobSystem.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JobSystem.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
